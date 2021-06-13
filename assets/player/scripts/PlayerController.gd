fae56ee11fead4245ec1df747214defc73a9c7c1
Stefan Talbot <codewellstudios@protonmail.com>
2021-06-13 10:18:24 -0400

    Added level: DevLvlFPSController
    - Cleaned up unused files

--- assets/player/scripts/PlayerController.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-var speed = 100
-
-func _process(delta):
-	if Input.is_key_down(KEY_W):
-		print_debug("Moving")
-		translation = translation + Vector3.FORWARD
-	pass # Replace with function body.
