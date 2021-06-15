d50d75222a4f6aa76e1595dccb092da4dadb72dc
Stefan Talbot <codewellstudios@protonmail.com>
2021-06-14 22:40:48 -0400

    Refactored: FPS Scripts
    * Refactored: FPSCamera
    * Refactored: FPSController
    * Clean up: FPSPlayer.tscn

--- assets/player/scripts/FPSCamera.gd
+++ assets/player/scripts/FPSCamera.gd
@@ -1,21 +1,30 @@
 extends Camera
 
-var mounseSensitivity = 0.1
-export(NodePath) var player
+export var mounseSensitivity : float = 0.1
+export(float, 0, 90, 0.1) var maxLookUp = 90
+export(float, -90, 0, 0.1) var minLookDown = -90
+
+export(NodePath) var playerNodePath : NodePath
+onready var player = get_node(playerNodePath)
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Lock mouse to game screen.
 
+# Gets mouse input and calls corresponding function.
 func _input(event):
 	if event is InputEventMouseMotion:
 		VerticalLook(event.relative.y)
 		HorizontalLook(event.relative.x)
 
+# Rotates this object around the x-axis
 func VerticalLook(mouseMovement):
 	rotation_degrees.x -= mouseMovement * mounseSensitivity
-	rotation_degrees.x = clamp(rotation_degrees.x, -90, 90)
+	rotation_degrees.x = clamp(rotation_degrees.x, minLookDown, maxLookUp) # Clamp our vertical rotation so we can't go upside down
 
+# Rotates an assigned object on the y-axis with mouse input.
 func HorizontalLook(mouseMovement):
-	if get_node(player) != null:
-		get_node(player).rotation_degrees.y -= mouseMovement * mounseSensitivity
-	
+	# Debugging
+	if player != null: # Allows the game to run without breaking.
+		player.rotation_degrees.y -= mouseMovement * mounseSensitivity 
+	else:
+		push_warning("No node assigned, did you forget to assign it in the inspector?")
--- assets/player/scripts/FPSController.gd
+++ assets/player/scripts/FPSController.gd
@@ -1,23 +1,26 @@
 extends KinematicBody
 
-export var speed : float = 5.0
-export var jumpSpeed : float = 6.5
+export(float, 0, 25, 0.01) var moveSpeed = 7.5
+export(float, 0, 25, 0.01) var jumpSpeed = 6.5
+export(float, -5, -0.1, 0.001) var fallSpeed = -0.326
+
 var velocity : Vector3 = Vector3()
 
-func _physics_process(delta):
+func _physics_process(_delta): # Use physics because this uses a KinematicBody.
 	move()
 
+# Move this object.
 func move():
 	calculate_movement()
-	move_and_slide(velocity,Vector3.UP, true)
+	move_and_slide(velocity, Vector3.UP, true)
 
+# Calculates this objects velocity.
 func calculate_movement():
-	velocity.x = get_input().x * speed
-	velocity.z = get_input().z * speed
-	velocity.y = gravity()
-	jump()
-	
+	velocity.x = get_input().x * moveSpeed
+	velocity.z = get_input().z * moveSpeed
+	velocity.y = gravity() + jump()
 
+# Returns player input as normalized Vector3.
 func get_input():
 	var inputDirection = Vector3()
 	if Input.is_action_pressed("move_forward"):
@@ -28,15 +31,20 @@ func get_input():
 		inputDirection -= transform.basis.x
 	if Input.is_action_pressed("move_left"):
 		inputDirection += transform.basis.x
-	inputDirection = inputDirection.normalized()
+
+	inputDirection = inputDirection.normalized() # Normalize vector so speed is constant in all directions.
 	return inputDirection
 
+# Returns a negative value to simulate "gravity".
 func gravity():
-	if self.is_on_floor():
+	if self.is_on_floor(): # Apply a small constant down force to keep grounded.
 		return -0.1
 	else:
-		return velocity.y-0.326
+		return velocity.y + fallSpeed
 
+# Returns a positive value to "jump".
 func jump():
 	if Input.is_action_just_pressed("jump") and self.is_on_floor():
-		velocity.y = jumpSpeed
+		return jumpSpeed
+	else:
+		return 0
