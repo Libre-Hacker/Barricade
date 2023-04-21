0c427ea45d924a47129137ce595b989ae9abae0b
Stefan Talbot <satalbot@protonmail.com>
2023-04-20 21:30:17 -0400

    UI and Particle Bug Fixes
    * Fixed: Prop health UI always visible.
    * Fixed: Healing particles affected by lights.

--- Project/assets/scripts/UIPropInfo.gd
+++ Project/assets/scripts/UIPropInfo.gd
@@ -6,12 +6,6 @@ onready var currentHealthLabel = get_node("VBoxContainer/HBoxContainer/CurrentHe
 onready var maxHealthLabel = get_node("VBoxContainer/HBoxContainer/MaxHealth")
 onready var propNameLabel = get_node("VBoxContainer/HBoxContainer/Name")
 
-func _process(_delta):
-	if(GameManager.isPaused or is_network_master() == false):
-		self.hide()
-	else:
-		self.show()
-
 func update_ui(propName = "", propHealth = 0.0, propMaxHealth = 0.0):
 	healthBar.max_value = propMaxHealth
 	healthBar.value = propHealth
